ecc7fae6c2a7c22f9e1f0023837b279509eb031f
hermes <hermes@local>
2026-08-12 00:56:08 +0800

    v0.2.27: bump versionCode 21 / versionName 0.2.27 (forgot in prev commit)

--- app-src/app/build.gradle.kts
+++ app-src/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.busung.s25uroot"
         minSdk = 33
         targetSdk = 36
-        versionCode = 20
-        versionName = "0.2.26"
+        versionCode = 21
+        versionName = "0.2.27"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
